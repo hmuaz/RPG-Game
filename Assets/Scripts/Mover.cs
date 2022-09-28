6aea50887f5f89ce7d0cdf2a3c51afcd6fb18008
hmuaz <muazsenol1@gmail.com>
2022-09-28 18:29:02 +0300

    Raycast

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -6,14 +6,22 @@ using UnityEngine.AI;
 public class Mover : MonoBehaviour
 {
     [SerializeField] Transform target;
+    Camera cam;
+    Ray lastRay;
     void Start()
     {
-        
+        cam = GameObject.Find("Main Camera").GetComponent<Camera>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        GetComponent<NavMeshAgent>().destination = target.position; 
+        GetComponent<NavMeshAgent>().destination = target.position;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            lastRay = cam.ScreenPointToRay(Input.mousePosition);
+        }
+        Debug.DrawRay(lastRay.origin, lastRay.direction * 100, Color.red);
     }
 }
