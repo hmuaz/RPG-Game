4054f0ff730ecf107bb03798d4aa28d5a1e4f54f
hmuaz <muazsenol1@gmail.com>
2022-10-13 17:31:20 +0300

    Visualize WayPoints

--- Assets/Scripts/Control/PatrolPath.cs
+++ Assets/Scripts/Control/PatrolPath.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,13 +7,30 @@ namespace RPG.Control
 {
     public class PatrolPath : MonoBehaviour
     {
-        const float wayPointGizmoRadius = 0.3f;
+        const float waypointGizmoRadius = 0.3f;
         private void OnDrawGizmos()
         {
             for (int i = 0; i < transform.childCount; i++)
             {
-                Gizmos.DrawSphere(transform.GetChild(i).position, wayPointGizmoRadius);
+                Gizmos.DrawSphere(transform.GetChild(i).position, waypointGizmoRadius);
+                int j = GetNextIndex(i);
+                Gizmos.DrawSphere(GetWaypoint(i), waypointGizmoRadius);
+                Gizmos.DrawLine(GetWaypoint(i), GetWaypoint(j));
             }
+        }
+
+        private int GetNextIndex(int i)
+        {
+            if (i + 1 == transform.childCount)
+            {
+                return 0;
+            }
+            return i + 1;
+        }
+
+        private Vector3 GetWaypoint(int i)
+        {
+            return transform.GetChild(i).position;
         }
     }
 }
