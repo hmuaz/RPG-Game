44c08d6c7a548fe3fa506ef9923bfa4da5e11176
hmuaz <muazsenol1@gmail.com>
2022-10-13 16:33:27 +0300

    Enemy Volta

---
+++ Assets/Scripts/Control/PatrolPath.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Control
+{
+    public class PatrolPath : MonoBehaviour
+    {
+        const float wayPointGizmoRadius = 0.3f;
+        private void OnDrawGizmos()
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                Gizmos.DrawSphere(transform.GetChild(i).position, wayPointGizmoRadius);
+            }
+        }
+    }
+}
