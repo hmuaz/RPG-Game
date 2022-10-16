cc04b9d7442565912b1c35ee5f21278431aa362a
hmuaz <muazsenol1@gmail.com>
2022-10-16 16:46:20 +0300

    Enter Cinematic

---
+++ Assets/Scripts/Cinematics/CinematicTrigger.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace RPG.Cinematics
+{
+    public class CinematicTrigger : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+            if(other.gameObject.tag == "Player")
+            {
+                GetComponent<PlayableDirector>().Play();
+                GetComponent<BoxCollider>().enabled = false;
+            }
+        }
+    }
+}
+
